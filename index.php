7143de136885ff8e73092bf0bd5dd34428955a72
ArslanR987 <133599385+ArslanR987@users.noreply.github.com>
2024-08-28 03:54:17 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title></title>
+
+	<!-- font awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+	<div id="navbar">
+		<div id="logo"><img src="assets/images/logo.png" ></div>
+		<button>
+			Book a Call
+			<i class="fa-solid fa-arrow-up fa-rotate-by " style="--fa-rotate-angle: 30deg;"></i>
+		</button>
+	</div>
+	<div id="hero-section">
+		<div id="hero-overlay"></div>
+		<img src="assets/images/hero-bg-design.png">
+		<div id="hero-content">
+			<p>Content That<br>Converts,Not<br>Just Clicks</p>
+			<p>Build an omni channel content strategy that consistently gains qualified<br>leads beyond the social media hamster wheel.</p>
+			<button>
+				<p>Work With Us</p>
+				<i class="fa-solid fa-arrow-up fa-rotate-by fa-xl" style="--fa-rotate-angle: 30deg;"></i>
+			</button>
+		</div>
+		<div id="hero-img-bg"> 
+			<img src="assets/images/hero-bg.png">
+		</div>
+	</div>
+</body>
+</html>
